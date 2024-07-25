ff4b9c6b67636c0805ce9cf0f38151e8f605baec
thienvo <111thienvo@gmail.com>
2024-07-25 17:31:54 +0700

    account group

---
+++ account-group-detail/account-group-detail.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { ShareModule } from 'src/app/share.module';
+import { AccountGroupDetailPage } from './account-group-detail.page';
+const routes: Routes = [
+  {
+    path: '',
+    component: AccountGroupDetailPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    ShareModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [AccountGroupDetailPage]
+})
+export class AccountGroupDetailPageModule { }
