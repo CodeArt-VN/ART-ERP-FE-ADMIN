aceec8cb71aca011e846adb6b5cb75a43121a2fa
thienvo <111thienvo@gmail.com>
2024-08-23 17:57:21 +0700

    update help detail

--- help-detail/help-detail.page.ts
+++ help-detail/help-detail.page.ts
@@ -40,15 +40,5 @@ export class HelpDetailPage extends PageBase {
     this.pageConfig.isDetailPage = true;
     //default code
     this._helpCode = 'help/' + this.route.snapshot.paramMap.get('code');
-    this.env.getStorage('lang').then((lang) => {
-      // get language then set the default code
-      this._helpCode = `help/${lang}/${this.route.snapshot.paramMap.get('code')}`;
-    });
-    this.env.getEvents().subscribe((data) => {
-      if (data.Code == 'app:changeLanguage') {
-        // listen event changeLanguage then set the default code
-        this._helpCode = `help/${data.Value}/${this.route.snapshot.paramMap.get('code')}`;
-      }
-    });
   }
 }
