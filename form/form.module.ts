7ef787c2fe1aa072b855930b17f0e27d1421d8d7
Hùng Vũ <hungvq@live.com>
2025-09-29 18:44:34 +0700

    clean code

--- form/form.module.ts
+++ form/form.module.ts
@@ -5,10 +5,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FormPage } from './form.page';
 import { ShareModule } from 'src/app/share.module';
-import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
 
 @NgModule({
-	imports: [IonicModule, CommonModule, FormsModule, NgOptionHighlightModule, ShareModule, RouterModule.forChild([{ path: '', component: FormPage }])],
+	imports: [IonicModule, CommonModule, FormsModule, ShareModule, RouterModule.forChild([{ path: '', component: FormPage }])],
 	declarations: [FormPage],
 })
 export class FormPageModule {}
--- help/help.module.ts
+++ help/help.module.ts
@@ -5,10 +5,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HelpPage } from './help.page';
 import { ShareModule } from 'src/app/share.module';
-import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
 
 @NgModule({
-	imports: [IonicModule, CommonModule, FormsModule, NgOptionHighlightModule, ShareModule, RouterModule.forChild([{ path: '', component: HelpPage }])],
+	imports: [IonicModule, CommonModule, FormsModule, ShareModule, RouterModule.forChild([{ path: '', component: HelpPage }])],
 	declarations: [HelpPage],
 })
 export class HelpPageModule {}
